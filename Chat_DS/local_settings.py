f9250517a9bdbe4b585117541bd778c2ffcd6709
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-06-08 22:38:34 +0300

    first commit

---
+++ Chat_DS/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat',
+        'USER': 'chat_admin',
+        'PASSWORD': 'chat_admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
