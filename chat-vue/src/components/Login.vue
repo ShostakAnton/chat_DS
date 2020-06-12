a175b804b70ef67d9b0332b75a790c7d31c2b793
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-06-12 20:44:14 +0300

    vue js - авторизация в приложении

---
+++ chat-vue/src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <input v-model="login" type="text" placeholder="Логин"/>
+        <input v-model="password" type="password" placeholder="Пароль"/>
+        <button @click="setLogin">Войти</button>
+    </div>
+</template>
+
+<script>
+    import $ from 'jquery'
+
+    export default {
+        name: "Login",
+        data() {
+            return{
+                login: '',
+                password: '',
+            }
+        },
+        methods: {
+            setLogin() {
+                $.ajax({
+                    url: 'http://127.0.0.1:8000/auth/token/create/',
+                    type: "POST",
+                    data: {             //данные которые передаем на бек-енд
+                        username: this.login,
+                        password: this.password
+                    },
+                    success: (response) => {
+                        alert("Спасибо что Вы с нами")
+                        console.log(response.data.attributes.auth_token)
+                        sessionStorage.setItem("auth_token", response.data.attributes.auth_token)       // сохранение с сессионное хранилище
+                        this.$router.push({name: "home"})
+                    },
+                    error: (response) => {
+                        if (response.status === 400) {
+                            alert("Логин или пароль не верен")
+                        }
+                    }
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
